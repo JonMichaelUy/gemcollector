4ce1bb2d9a6026bf58a879d38b6a9af3a094df95
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-07-05 22:53:09 -0400

    create Gem model and remove hardcoded data and create new data using Gem model

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Gem
 # Create your views here.
 from django.http import HttpResponse
 
@@ -13,20 +13,27 @@ def about(request):
 
 
 
-class Gem: 
-  def __init__(self, name, crystal_system, color, hardness, specific_gravity):
-    self.name = name
-    self.crystal_system = crystal_system
-    self.color = color
-    self.hardness = hardness
-    self.specific_gravity = specific_gravity
+# class Gem: 
+#   def __init__(self, name, crystal_system, color, hardness, specific_gravity):
+#     self.name = name
+#     self.crystal_system = crystal_system
+#     self.color = color
+#     self.hardness = hardness
+#     self.specific_gravity = specific_gravity
 
-gems = [
-  Gem('Diamond', 'Cubic', 'white to black, colorless, yellow, pink, red, blue, brown', 10, '3.4-3.5'),
-  Gem('Ruby', 'Hexagonal-trigonal', 'red', 9, '4.0-4.1'),
-  Gem('Sapphire', 'Hexagonal-trigonal', 'occurs in most colors', 9, '4.0-4.1'),
-  Gem('Zircon', 'Tetragonal', 'colorless, brown, red, yellow, orange, blue, green', 7.5, '4.6-4.7')
-]
+# gems = [
+#   Gem('Diamond', 'Cubic', 'white to black, colorless, yellow, pink, red, blue, brown', 10, '3.4-3.5'),
+#   Gem('Ruby', 'Hexagonal-trigonal', 'red', 9, '4.0-4.1'),
+#   Gem('Sapphire', 'Hexagonal-trigonal', 'occurs in most colors', 9, '4.0-4.1'),
+#   Gem('Zircon', 'Tetragonal', 'colorless, brown, red, yellow, orange, blue, green', 7.5, '4.6-4.7')
+# ]
+
 def gems_index(request):
   # return HttpResponse('<h1>About the gem collector</h1>')
+  gems = Gem.objects.all()
   return render(request, 'gems/index.html', { 'gems': gems})
+
+
+
+
+  # Gem.objects.create(name='Diamon', crystal_system='Cubic', color='white to black, colorless, yellow, pink, red, blue, brown', hardness=10, specific_gravity='3.4-3.5')
