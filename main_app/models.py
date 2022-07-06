0b6eabb625194f55d0d7c8d8f45d73fecb042349
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-07-05 22:33:03 -0400

    add index page and hardcoded data for visual

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Gem(models.Model):
+  name = models.CharField(max_length=100)
+  crystal_system = models.CharField(max_length=100)
+  color = models.TextField(max_length=250)
+  hardness = models.IntegerField()
+  specific_gravity = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,23 @@ def home(request):
 def about(request):
   # return HttpResponse('<h1>About the gem collector</h1>')
   return render(request, 'about.html')
+
+
+
+class Gem: 
+  def __init__(self, name, crystal_system, color, hardness, specific_gravity):
+    self.name = name
+    self.crystal_system = crystal_system
+    self.color = color
+    self.hardness = hardness
+    self.specific_gravity = specific_gravity
+
+gems = [
+  Gem('Diamond', 'Cubic', 'white to black, colorless, yellow, pink, red, blue, brown', 10, '3.4-3.5'),
+  Gem('Ruby', 'Hexagonal-trigonal', 'red', 9, '4.0-4.1'),
+  Gem('Sapphire', 'Hexagonal-trigonal', 'occurs in most colors', 9, '4.0-4.1'),
+  Gem('Zircon', 'Tetragonal', 'colorless, brown, red, yellow, orange, blue, green', 7.5, '4.6-4.7')
+]
+def gems_index(request):
+  # return HttpResponse('<h1>About the gem collector</h1>')
+  return render(request, 'gems/index.html', { 'gems': gems})
