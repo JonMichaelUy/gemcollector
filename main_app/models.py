db02bc56a3810cad698536c00c6fe402e64f3500
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-07-06 21:10:32 -0400

    added new model called Polishing

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.urls import reverse
+
+
+CLEANERS = (
+  ('C', 'Cloth'),
+  ('S', 'Silica'),
+  ('A', 'Ammonia')
+)
 # Create your models here.
+
+
 class Gem(models.Model):
   name = models.CharField(max_length=100)
   crystal_system = models.CharField(max_length=100)
@@ -13,3 +22,14 @@ class Gem(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'gem_id': self.id})
+
+class Polishing(models.Model):
+  date = models.DateField()
+  cleaner = models.CharField(
+    max_length=1,
+    choices=CLEANERS,
+    default=CLEANERS[0][0]
+  )
+  gem = models.ForeignKey(Gem, on_delete=models.CASCADE)
+  def __str__(self):
+    return f"{self.get_cleaner_display()} on {self.date}"
